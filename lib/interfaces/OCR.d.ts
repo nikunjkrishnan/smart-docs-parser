e31d9e1c9200933b250d72a9dcc9ccb13edbd8e5
nikunj <nikunjkrishnan@urbanclap.com>
2020-04-28 00:02:04 +0530

    code refractor

---
+++ lib/interfaces/OCR.d.ts
@@ -0,0 +1,11 @@
+export interface ExtractDocumentTypeRequest {
+    document_url: string;
+    api_key: string;
+    languages?: string[];
+}
+export interface ExtractDocumentTypeResponse {
+    raw_text: Array<string>;
+}
+export interface CustomOCR {
+    extractDocumentText(params: ExtractDocumentTypeRequest): ExtractDocumentTypeResponse;
+}
